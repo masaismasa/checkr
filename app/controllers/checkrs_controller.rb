c40cb9dba9116cbbe4edfd232ab9b2696ea90f30
toshimasa <masaismasa@gmail.com>
2017-06-01 08:12:25 +0000

    検索条件変更

--- app/controllers/checkrs_controller.rb
+++ app/controllers/checkrs_controller.rb
@@ -10,7 +10,12 @@ class CheckrsController < ApplicationController
   end
   
   def create
+
   p "start********************************"
+  p params[:data]
+  p params[:data][:room_type]
+  p params[:data][:rent_price]
+  p params[:data][:size]
   
   #訪問先アドレス
   site_config("http://myhome.nifty.com/rent/tokyo/adachiku/oyata")
@@ -19,25 +24,52 @@ class CheckrsController < ApplicationController
   # アクセス
   visit('')
   
-  #検索条件を下記で指定
-  select('賃料が安い順', :from => 'sort1')
-  qwery_price = '10万円以上'
-  select(qwery_price, :from => 'r1')
-  check "r20-3"
+
   
+  #検索条件を下記で指定
+
+  select('賃料が安い順', :from => 'sort1')
+  
+  # if params[:data][:rent_price].to_f >= 10
+  #   qwery_price = '10万円以上'
+  #   elsif params[:data][:rent_price].to_f >= 5
+  #         qwery_price = '5万円以上'
+  #       else
+  #         qwery_price = '下限なし'
+  #   end
+  # puts qwery_price
+  # select(qwery_price, :from => 'r1')
+  
+  qwery_price = "#{params[:data][:rent_price].to_f.floor}万円以上"
+  puts qwery_price
+ 
+  if params[:data][:size].to_f >= 50
+    qwery_size = '50平米以上'
+    elsif params[:data][:size].to_f >= 20
+          qwery_size = '20平米以上'
+        else
+          qwery_size = '指定なし'
+    end
+   puts qwery_size
+  select(qwery_size, :from => 'r10')
+
+
   sleep 3 
+  
   #tableのtbodyを取得し、内部のtrを配列で取得
   contents = find('#searchResultList > tbody').all('tr')
-  
-  # ループで要素を取り出す
-  # header1 要素3の倍数なので、each_with_indexを使って余１を抽出
+  @contents = []
   contents.each_with_index do |content, n|
     if n % 3 == 1
-      #とりあえず要素をテキスト化
+      # result = Result .new
+      # result.address = content.find()
+      # result.save
       p content.text
-      #後でこのなかで要素を取得
+      @contents << content.text
+      end
     end
+  
+  render "checkrs/index"
+  
   end
-
-end
 end
